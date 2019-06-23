54f9e2f99366d157b66e0224e3eeb5466a22a5bb
lavumi <35640315+lavumi@users.noreply.github.com>
2019-06-24 00:06:47 +0900

    position change

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ var Main = (function (){
         for( var i = -1 ; i < 2 ; i ++){
             for( var j = 0 ; j< 3 ; j ++){
                 tempModel = new ModelBase(filename);
-                tempModel.moveTo( i * 2, j , j * 1.5  );
+                tempModel.moveTo( i * 2, j - 2, j * 1.5  );
 
                 objects.push(tempModel);
 
